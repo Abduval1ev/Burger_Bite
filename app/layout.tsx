b8b2acf6720b15246cc47b061ba5d41b5ef29e62
Abdumannof <160102960+Abduval1ev@users.noreply.github.com>
2024-04-02 15:28:51 +0300

    #fix #css #ts Navigation panel vorstka va funksional qiymatga keltirildi.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from "next";
 import { Barlow_Condensed } from "next/font/google";
 import "./globals.css";
+import ResponsiveNav from "./components/Navigation/ResponsiveNav";
 
 const barlow_Condensed = Barlow_Condensed({
   weight: ["300", "400", "500", "600", "700", "800"],
   subsets: ["latin"],
-});
+})
+
 
 export const metadata: Metadata = {
   title: "Burger | A healty Food",
@@ -21,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={barlow_Condensed.className}>
+        <ResponsiveNav />
         {children}
       </body>
     </html>
