81f6e778d1511270bbe38ff8ca1f8950b73e3630
Abdumannof <160102960+Abduval1ev@users.noreply.github.com>
2024-04-04 01:06:04 +0300

    #fix #css #ts Features panel vorstka qilindi va adaptiv holat berildi.

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './Hero/Hero'
 import Features from './Features/Features'
+import PopularBurger from './PopularBurger/PopularBurger'
 
 export default function Home() {
   return (
     <div className='overflow-hidden'>
       <Hero />
       <Features />
+      <PopularBurger />
     </div>
   )
 }
---
+++ app/components/PopularBurger/BurgerCart.tsx
@@ -0,0 +1,88 @@
+
+import React from 'react'
+import Image from 'next/image';
+
+interface Props {
+    title: string;
+    image: string;
+    price: string;
+    reviews: string;
+}
+
+// const imageFunc = (source: string): JSX.Element => {
+//     return <Image src={source} alt='Image' />
+// }
+
+// const getImageData = [
+//     {
+//         id: 1,
+//         image: imageFunc('/images/b1.png'),
+//         text: "salom 1",
+//         price: "$12.00",
+//         reviews: "saas",
+//     },
+//     {
+//         id: 2,
+//         image: imageFunc('/images/b2.png'),
+//         text: "salom 1",
+//         price: "$12.00",
+//         reviews: "saas",
+//     },
+//     {
+//         id: 3,
+//         image: imageFunc('/images/b3.png'),
+//         text: "salom 1",
+//         price: "$12.00",
+//         reviews: "saas",
+//     },
+//     {
+//         id: 4,
+//         image: imageFunc('/images/b4.png'),
+//         text: "salom 1",
+//         price: "$12.00",
+//         reviews: "saas",
+//     },
+//     {
+//         id: 5,
+//         image: imageFunc('/images/b5.png'),
+//         text: "salom 1",
+//         price: "$12.00",
+//         reviews: "saas",
+//     },
+//     {
+//         id: 6,
+//         image: imageFunc('/images/b6.png'),
+//         text: "salom 1",
+//         price: "$12.00",
+//         reviews: "saas",
+//     },
+//     {
+//         id: 7,
+//         image: imageFunc('/images/b7.png'),
+//         text: "salom 1",
+//         price: "$12.00",
+//         reviews: "saas",
+//     },
+//     {
+//         id: 8,
+//         image: imageFunc('/images/b8.png'),
+//         text: "salom 1",
+//         price: "$12.00",
+//         reviews: "saas",
+//     },
+//     {
+//         id: 9,
+//         image: imageFunc('/images/b9.png'),
+//         text: "salom 1",
+//         price: "$12.00",
+//         reviews: "saas",
+//     },
+// ]
+
+export default function BurgerCart({ title, price, image, reviews }: Props) {
+    return (
+        <>
+
+        </>
+    )
+}
